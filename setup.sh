15312bfe353d45a6749cc98c8ebcb3dd017f5cf0
Johnny You <johnnyou1873@gmail.com>
2025-11-30 22:23:47 -0800

    minor optimizations

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
-echo "Setting up Python 3.13 virtual environment..."
-uv venv -p 3.13
+echo "Setting up Python 3.10 virtual environment..."
+uv venv -p 3.10
 source .venv/bin/activate
 uv pip install pip
-pip install -r requirements.txt
+uv pip install -e .
+uv pip install -r requirements.txt
 echo "Virtual environment setup complete. Please add allDatasets.pkl to the data/ directory. Run 'python src/train.py experiment=example' to start training."
