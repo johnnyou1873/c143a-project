8f9a9b0d5ebca08cf0bb0c4eb77040f676f0ec1f
Johnny You <johnnyou1873@gmail.com>
2025-12-01 22:18:06 -0800

    push phoneme model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from .speech_module import SpeechModule
-from .llm_module import LLMSpeechModule
+from .ps2s_module import PS2SSpeechModule
 
-__all__ = ["SpeechModule", "LLMSpeechModule"]
+__all__ = ["SpeechModule", "PS2SSpeechModule"]
